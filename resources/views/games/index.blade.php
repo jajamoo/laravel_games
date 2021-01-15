f369ac94b96e4cba25d15ee817c0fd4fc2a6b121
Esanul Kahn <esanul.kahn@automationanywhere.com>
2021-01-15 15:58:36 -0500

    Blade fixes

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,10 +1,10 @@
 @section('content')
-    @foreach ($games as $game)
-        <ul>
+    <ul>
+        @foreach ($games ?? '' as $game)
             <li>
                 <a href="{{ route('show_game', [$id]) }}">{{ $game->title }}</a>
             <li>
-        </ul>
-    @endforeach
+        @endforeach
+    </ul>
     <a href="{{route('add_games')}}" class="btn btn-primary">Add Games</a>
 @endsection
